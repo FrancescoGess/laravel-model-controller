35fa5e5549e1eb9938ba77218963f9f4f7a5f80b
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-03-18 15:57:53 +0100

    fix funzione

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -8,8 +8,8 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index() {
-
+    public function index()
+    {
         $movies = Movie::all();
 
         return view('pages.welcome', compact('movies'));
